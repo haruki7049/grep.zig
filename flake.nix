edeae7ffacb03c26de1db57013cf988f70f3217b
haruki7049 <tontonkirikiri@gmail.com>
2024-09-08 20:53:46 +0900

    feat(repo): Init

---
+++ flake.nix
@@ -0,0 +1,59 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, treefmt-nix, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        lib = pkgs.lib;
+        stdenv = pkgs.stdenv;
+        zig = pkgs.zig_0_13;
+        treefmtEval = treefmt-nix.lib.evalModule pkgs ./treefmt.nix;
+
+        grep-zig = stdenv.mkDerivation {
+          pname = "grep-zig";
+          version = "dev";
+
+          src = lib.cleanSource ./.;
+
+          nativeBuildInputs = [
+            zig.hook
+          ];
+
+          zigBuildInputs = [
+            "-Doptimize=Debug"
+          ];
+        };
+      in
+      {
+        packages = {
+          inherit grep-zig;
+          default = grep-zig;
+        };
+
+        formatter = treefmtEval.config.build.wrapper;
+
+        checks = {
+          formatting = treefmtEval.config.build.check self;
+        };
+
+        devShells.default = pkgs.mkShell {
+          packages = [
+            # Ziglang
+            zig
+            pkgs.zls
+
+            # Nixlang
+            pkgs.nil
+          ];
+
+          shellHook = ''
+            export PS1="\n[nix-shell:\w]$ "
+          '';
+        };
+      });
+}
