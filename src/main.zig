70fc16435356d34c0448c99061b91c348b3f8547
haruki7049 <tontonkirikiri@gmail.com>
2024-09-13 13:11:52 +0900

    hoge

--- src/main.zig
+++ src/main.zig
@@ -1,3 +1,9 @@
 const std = @import("std");
+const mem = std.mem;
 
 pub fn main() !void {}
+
+fn display_context(file_name: []const u8) !void {
+    var buffer: [100]u8 = undefined;
+    const file: []u8 = try std.fs.cwd().readFile(file_name, buffer);
+}
